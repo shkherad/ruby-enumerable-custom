cbd48db0170270fc5899b6d4658ea9684432720e
shkherad <shkherad@bu.edu>
2016-06-10 14:08:42 -0400

    class demo notes updated
    class demo notes updated

--- lib/card.rb
+++ lib/card.rb
@@ -1,5 +1,6 @@
 # A simple representation of a playing card.
 class Card
+  include Comparable
   SUITS = %w(C D H S).freeze
   RANKS = [(2..10).to_a, %w(J Q K A)].flatten
 
@@ -18,4 +19,24 @@ class Card
   def to_s
     "#{rank}:#{suit}"
   end
+
+  def <=> (other)
+
+    suit_comparison = SUITS.index(suit) <=> SUITS.index(other.suit)
+    rank_comparison = RANKS.index(rank) <=> RANKS.index(other.rank)
+    #compare by suit
+    if suit_comparison == 0
+      rank_comparison
+    else
+      suit_comparison
+
+  end
+
+
 end
+
+c1 = Card.new(5, "S")
+c2 = Card.new(10
+,"S")
+
+puts c1 == c2
--- lib/stepped_range.rb
+++ lib/stepped_range.rb
@@ -1,3 +1,28 @@
-#
+
+
 class SteppedRange
+  include Enumerable
+
+  attr_reader :start, :stop, :step
+
+  def each
+    current = start
+    while current <= stop
+      yield current
+      current += step
+    end
+  end
+
+  #only assigns step to 1 if step is not given as an input
+  def initialize (start, stop, step=1)
+    @start = start
+    @stop = stop
+    @step = step
+  end
+
+
+
 end
+
+new_range = SteppedRange.new(1,15,3).to_a
+p new_range
